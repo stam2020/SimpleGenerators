e82e63c941d5c88f18fb301e983ac48edb65ebb7
BuildTools <unconfigured@null.spigotmc.org>
2021-07-19 12:49:28 +0300

    loading project into git

---
+++ src/plainplaying/plugin/gens/CommandHandler.java
@@ -0,0 +1,71 @@
+package plainplaying.plugin.gens;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import oshi.util.tuples.Pair;
+
+import java.util.*;
+
+public class CommandHandler implements CommandExecutor, TabExecutor {
+    private Economy eco;
+    public CommandHandler(Economy eco){
+        this.eco = eco;
+    }
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        Player player = (Player) commandSender;
+        if (command.getName().equalsIgnoreCase("sell")){
+            Inventory playerInv = player.getInventory();
+            Pair<Integer,Integer> answerData = SimpleGenerators.sellInventory(playerInv,eco,player,1);
+            HashMap<String,String> env = new HashMap<>();
+            env.put("a",answerData.getA().toString());
+            env.put("n",answerData.getB().toString());
+            SimpleGenerators.getInstance().executeMessage(SimpleGenerators.config.getConfigurationSection("messages.sell_with_command"),player,env);
+        }else if (command.getName().equalsIgnoreCase("SimpleGenerators")){
+            switch (strings[0]){
+                case "reload" -> {
+                    if (player.hasPermission("simplegenerators.admin.reload")) {
+                        SimpleGenerators.getInstance().onEnableRun();
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aPlugin reloaded successfully"));
+                    }else{
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cNo permissions"));
+                    }
+                }
+                case "credits" -> player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&bPlugin made by &2PlainPlaying"));
+                case "info" -> player.sendMessage(ChatColor.translateAlternateColorCodes('&',"Currently running version &81.0.0"));
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (command.getName().equalsIgnoreCase("SimpleGenerators")){
+            List<String> possibleCompletions = Arrays.asList("reload","credits","info");
+            String input = strings[0].toLowerCase();
+            List<String> completions = null;
+            for (String completion : possibleCompletions){
+                if (completion.startsWith(input)){
+                    if (completions == null){
+                        completions = new ArrayList<>();
+                    }
+                    completions.add(completion);
+                }
+            }
+            if (completions != null){
+                Collections.sort(completions);
+            }
+            return completions;
+        }
+        return null;
+    }
+}
---
+++ src/plainplaying/plugin/gens/SimpleGenerators.java
@@ -0,0 +1,378 @@
+package plainplaying.plugin.gens;
+
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.block.Block;
+import org.bukkit.block.Container;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+import oshi.util.tuples.Pair;
+
+import java.io.File;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+public class SimpleGenerators extends JavaPlugin implements Listener {
+    private File configFile = new File(getDataFolder(),"config.yml");
+    private Connection conn;
+    public String host, database, username, password;
+    public int port;
+    private ConfigurationSection generators;
+    private static Economy eco;
+    public static Logger logger;
+    public static FileConfiguration config;
+    private static boolean offlineGeneration;
+    private static SimpleGenerators instance;
+    HashMap<Player,Long> lastClickDelay = new HashMap<>();
+    int id;
+    @Override
+    public void onLoad() {
+        getLogger().info("Plugin loaded successfully");
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+        if (!setupEconomy() ) {
+            getLogger().severe("Disabled due to no Vault dependency found!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        getCommand("sell").setExecutor(new CommandHandler(eco));
+        getCommand("SimpleGenerators").setExecutor(new CommandHandler(eco));
+        getCommand("SimpleGenerators").setTabCompleter(new CommandHandler(eco));
+        mysqlSetup();
+        getServer().getPluginManager().registerEvents(this,this);
+        onEnableRun();
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Simple generators is down.");
+    }
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        Block placedBlock = e.getBlock();
+        Player player = e.getPlayer();
+        ItemMeta heldItem = player.getInventory().getItemInMainHand().getItemMeta();
+        for (String gen : generators.getKeys(false)){
+            ConfigurationSection currentGen = getConfig().getConfigurationSection("generators."+gen);
+            if (placedBlock.getType().equals(Material.matchMaterial(currentGen.getString("block")))){
+                if (heldItem.getDisplayName().equals(ChatColor.translateAlternateColorCodes('&',currentGen.getString("name")))){
+                    try {
+                        String countGensQuery = "SELECT count(*) FROM gens WHERE placer=\""+player.getUniqueId().toString()+"\"";
+                        Statement countGensStmt = conn.createStatement();
+                        ResultSet countGens = countGensStmt.executeQuery(countGensQuery);
+                        countGens.next();
+                        int genPlaced = countGens.getInt(1);
+                        while (countGens.next()){
+                            getLogger().info(countGens.getString("type"));
+                        }
+                        int maxGens = 0;
+                        Iterator<PermissionAttachmentInfo> perms = player.getEffectivePermissions().iterator();
+                        while (perms.hasNext()){
+                            PermissionAttachmentInfo perm = perms.next();
+                            if (perm.getPermission().startsWith("simplegenerators.max_gens.")){
+                                maxGens = Math.max(maxGens,Integer.parseInt(perm.getPermission().substring(26)));
+                            }
+                        }
+                        if (genPlaced <= maxGens-1) {
+
+                            String query = "INSERT INTO gens(location,type,placer) values(?,?,?)";
+                            PreparedStatement stmt = conn.prepareStatement(query);
+                            stmt.setString(1, parseLocation(placedBlock.getLocation()));
+                            stmt.setString(2, gen);
+                            stmt.setString(3, player.getUniqueId().toString());
+                            stmt.execute();
+                            executeMessage(getConfig().getConfigurationSection("messages.gen_place"),player,new HashMap<>());
+                        }else{
+                            executeMessage(getConfig().getConfigurationSection("messages.max_gen"),player,new HashMap<>());
+                            e.setCancelled(true);
+                        }
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                }
+            }
+        }
+        if (placedBlock.getType().equals(Material.CHEST)){
+            try {
+                String query = "INSERT INTO chests(location,player) values(?,?)";
+                PreparedStatement addChest = conn.prepareStatement(query);
+                addChest.setString(1,parseLocation(placedBlock.getLocation()));
+                addChest.setString(2,player.getUniqueId().toString());
+                addChest.execute();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+        }
+    }
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e){
+        Block brokenBlock = e.getBlock();
+        if (brokenBlock.getType().equals(Material.CHEST)){
+            try {
+                String query = "DELETE FROM chests WHERE location=?";
+                PreparedStatement addChest = conn.prepareStatement(query);
+                addChest.setString(1,parseLocation(brokenBlock.getLocation()));
+                addChest.execute();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+        }
+    }
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e){
+        Player player = e.getPlayer();
+        ItemStack mainHandItem = player.getInventory().getItemInMainHand();
+        if (e.getClickedBlock() != null) {
+            String brokenBlockLocation = parseLocation(e.getClickedBlock().getLocation());
+            try {
+                String query = "SELECT location,type,placer FROM gens";
+                Statement stmt = conn.createStatement();
+                ResultSet ans = stmt.executeQuery(query);
+                while (ans.next()) {
+                    String location = ans.getString("location");
+                    String placer = ans.getString("placer");
+                    int type = ans.getInt("type");
+                    if (location.equals(brokenBlockLocation) && placer.equals(player.getUniqueId().toString())) {
+                        if (e.getAction() == Action.LEFT_CLICK_BLOCK) {
+                            e.getClickedBlock().setType(Material.AIR);
+                            e.setCancelled(true);
+                            ConfigurationSection itemInfo = getConfig().getConfigurationSection("generators." + type);
+                            ItemStack genItem = new ItemStack(Material.matchMaterial(itemInfo.getString("block")));
+                            ItemMeta genItemMeta = genItem.getItemMeta();
+                            genItemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', itemInfo.getString("name")));
+                            genItem.setItemMeta(genItemMeta);
+                            giveItemsNaturally(player, genItem, e.getClickedBlock().getLocation());
+                            String deletionQuery = "DELETE FROM gens WHERE location='" + brokenBlockLocation + "'";
+                            PreparedStatement delete = conn.prepareStatement(deletionQuery);
+                            delete.execute();
+                            executeMessage(getConfig().getConfigurationSection("messages.gen_break"),player,new HashMap<>());
+                            break;
+                        }else if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                            if (player.isSneaking()) {
+                                if (System.currentTimeMillis() - (lastClickDelay.containsKey(player) ? lastClickDelay.get(player) : 0) > 100) {
+                                    lastClickDelay.put(player, System.currentTimeMillis());
+                                    if (eco.getBalance(player) >= getConfig().getInt("generators."+type+".upgrade_price")) {
+                                        eco.withdrawPlayer(player, getConfig().getInt("generators."+type+".upgrade_price"));
+                                        if (getConfig().getInt("max_gen") == type) {
+                                            executeMessage(getConfig().getConfigurationSection("messages.highest_gen"),player,new HashMap<>());
+                                            return;
+                                        }
+                                        e.getClickedBlock().setType(Material.matchMaterial(getConfig().getString("generators." + (type + 1) + ".block")));
+                                        String genUpdate = "UPDATE gens SET type=? WHERE location=?";
+                                        PreparedStatement update = conn.prepareStatement(genUpdate);
+                                        update.setInt(1, type + 1);
+                                        update.setString(2, location);
+                                        update.execute();
+                                        executeMessage(getConfig().getConfigurationSection("messages.gen_upgrade"),player,new HashMap<>());
+                                    }else{
+                                        executeMessage(getConfig().getConfigurationSection("messages.not_enough_money"),player,new HashMap<>());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK){
+            for (String sellwand : getConfig().getConfigurationSection("sellwands").getKeys(false)) {
+                ConfigurationSection currentSellwand = getConfig().getConfigurationSection("sellwands."+sellwand);
+                if (mainHandItem.getType().equals(Material.matchMaterial(currentSellwand.getString("item"))) && mainHandItem.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&',currentSellwand.getString("name")))){
+                    if (e.getClickedBlock().getType() == Material.CHEST){
+                        try {
+                            String getPlacer = "SELECT player FROM chests WHERE location=\""+parseLocation(e.getClickedBlock().getLocation())+"\"";
+                            Statement getPlacerStmt = conn.createStatement();
+                            ResultSet ans = getPlacerStmt.executeQuery(getPlacer);
+                            ans.next();
+                            String placerUUID = ans.getString("player");
+                            Player placer = getServer().getPlayer(UUID.fromString(placerUUID));
+                            e.setCancelled(true);
+                            if (placer.equals(player)) {
+                                Pair<Integer, Integer> answerData = sellInventory(((Container) e.getClickedBlock().getState()).getInventory(), eco, player, currentSellwand.getInt("multiplier"));
+                                HashMap<String,String> env = new HashMap<>();
+                                env.put("a",answerData.getA().toString());
+                                env.put("n",answerData.getB().toString());
+                                executeMessage(getConfig().getConfigurationSection("messages.sell_with_wand"),player,env);
+                            }else{
+                                executeMessage(getConfig().getConfigurationSection("messages.cant_sell_contents"),player,new HashMap<>());
+                            }
+                        } catch (SQLException throwables) {
+                            throwables.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public static SimpleGenerators getInstance(){
+        return instance;
+    }
+    public void mysqlSetup(){
+        host = "localhost";
+        port = 3306;
+        username = "root";
+        database = "SimpleGenerators";
+        password = "";
+        try{
+            synchronized (this) {
+                if (getConn() != null && !getConn().isClosed()) {
+                    return;
+                }
+                Class.forName("com.mysql.jdbc.Driver");
+                setConn(DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+database,"root",""));
+            }
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        eco = rsp.getProvider();
+        return eco != null;
+    }
+    public Connection getConn() {
+        return conn;
+    }
+
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+    public String parseLocation(Location loc){
+        return loc.getWorld().getName()+" "+loc.getX()+" "+loc.getY()+" "+loc.getZ();
+    }
+    public Location unparseLocation(String loc){
+        String[] locationData = loc.split(" ");
+        return new Location(getServer().getWorld(locationData[0]),Double.parseDouble(locationData[1]),Double.parseDouble(locationData[2]),Double.parseDouble(locationData[3]));
+    }
+    public void giveItemsNaturally(Player player, ItemStack items, Location dropLocation){
+        Inventory playerInv = player.getInventory();
+        int firstAvailable = playerInv.firstEmpty();
+        for (ItemStack item : playerInv) {
+            if (item != null) {
+                if (item.isSimilar(items) && item.getItemMeta().getDisplayName().equals(items.getItemMeta().getDisplayName())) {
+                    int newAmount = item.getAmount() + items.getAmount();
+                    if (newAmount <= 64) {
+                        item.setAmount(newAmount);
+                        return;
+                    } else {
+                        item.setAmount(64);
+                        items.setAmount(newAmount - 64);
+
+                    }
+                }
+            } else {
+                if (firstAvailable != -1) {
+                    playerInv.setItem(firstAvailable, items);
+                }else{
+                    player.getWorld().dropItemNaturally(dropLocation, items);
+                }
+                return;
+            }
+        }
+        player.getWorld().dropItemNaturally(dropLocation, items);
+    }
+    public static Pair<Integer,Integer> sellInventory(Inventory inv, Economy eco, Player player, int multiplier){
+        int itemsSold = 0;
+        int sellPrice = 0;
+        Set<String> sellableItems = SimpleGenerators.config.getConfigurationSection("items").getKeys(false);
+        for (ItemStack item : inv){
+            if (item != null) {
+                String itemId = item.getType().name();
+                for (String sellableItem : sellableItems) {
+                    if (itemId.equalsIgnoreCase(sellableItem)) {
+                        itemsSold += item.getAmount();
+                        sellPrice += SimpleGenerators.config.getInt("items."+sellableItem+".sell_price")*item.getAmount();
+                        item.setAmount(0);
+                    }
+                }
+            }
+        }
+        eco.depositPlayer(player,sellPrice*multiplier);
+        return new Pair<>(sellPrice*multiplier,itemsSold);
+    }
+    public void executeMessage(ConfigurationSection message, Player player, HashMap<String,String> env){
+        switch (message.getString("type")){
+            case "message" -> player.sendMessage(ChatColor.translateAlternateColorCodes('&',translateEnv(message.getString("message"),env)));
+            case "title" ->  player.sendTitle(ChatColor.translateAlternateColorCodes('&',translateEnv(message.getString("title"),env)),ChatColor.translateAlternateColorCodes('&',translateEnv(message.getString("subtitle"),env)),message.getInt("fade_in"),message.getInt("stay"),message.getInt("fade_out"));
+            case "actionbar" -> {getLogger().info("yes");player.spigot().sendMessage(ChatMessageType.ACTION_BAR,new TextComponent(ChatColor.translateAlternateColorCodes('&',translateEnv(message.getString("contents"),env))));}
+            case "none" -> {}
+        }
+    }
+    private String translateEnv(String message, HashMap<String,String> env){
+        for (String key : env.keySet()){
+            message = message.replace("%"+key+"%",env.get(key));
+        }
+        return message;
+    }
+    public void onEnableRun(){
+        generators = getConfig().getConfigurationSection("generators");
+        getLogger().info("Simple generators is setup!");
+        if (!configFile.exists()){
+            saveDefaultConfig();
+        }
+        this.reloadConfig();
+        offlineGeneration = getConfig().getBoolean("generate_offline");
+        logger = getLogger();
+        config = getConfig();
+        try {
+            getServer().getScheduler().cancelTask(id);
+        } catch (Exception ignored){}
+        id = getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            try {
+                String query = "SELECT location,type,placer FROM gens";
+                Statement gensGetter = conn.createStatement();
+                ResultSet gens = gensGetter.executeQuery(query);
+                while (gens.next()){
+                    OfflinePlayer playerGen = getServer().getOfflinePlayer(UUID.fromString(gens.getString("placer")));
+                    if (offlineGeneration || playerGen.isOnline()) {
+                        ConfigurationSection itemInfo = getConfig().getConfigurationSection("generators." + gens.getString("type"));
+                        Location dropLoc = unparseLocation(gens.getString("location"));
+                        dropLoc.add(0, 1, 0);
+                        ItemStack drop = new ItemStack(Material.matchMaterial(itemInfo.getString("drop")));
+                        dropLoc.getWorld().dropItemNaturally(dropLoc, drop);
+                    }
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+        },0,getConfig().getInt("drop_frequency"));
+    }
+}
